5cbb9da809549795bbd7ef6396123374f5a540ab
skabyy <skabyy@gmail.com>
2018-12-14 08:06:53 +0800

    calc test

---
+++ Nccc.Tests/Calculator/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nccc.Tests.Calculator
+{
+    [TestClass]
+    public class Calculator
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var x = _Calc("(5.1+2)*3+-2^3^2");
+            var x0 = (5.1 + 2) * 3 - Math.Pow(2, Math.Pow(3, 2));
+            Console.WriteLine(x);
+            Assert.AreEqual(x0, x);
+        }
+
+        private double _Calc(string exp)
+        {
+            var parser = NcParser.LoadFromAssembly(Assembly.GetExecutingAssembly(), "Nccc.Tests.Calculator.calculator.grammer");
+            var pr = parser.ScanAndParse(exp);
+            if (!pr.IsSuccess())
+            {
+                throw new ArgumentException($"Parsing fail: {pr.ToSExp().ToPrettyString()}");
+            }
+            return _ValueOf(pr.Nodes.First());
+        }
+
+        private double _ValueOf(Node node)
+        {
+            return node.Match<double>(type =>
+            {
+                type("par", es => _ValueOf(es.First()));
+                type("add", es => _ValueOf(es[0]) + _ValueOf(es[1]));
+                type("sub", es => _ValueOf(es[0]) - _ValueOf(es[1]));
+                type("mul", es => _ValueOf(es[0]) * _ValueOf(es[1]));
+                type("div", es => _ValueOf(es[0]) / _ValueOf(es[1]));
+                type("pow", es => Math.Pow(_ValueOf(es[0]), _ValueOf(es[1])));
+                type("neg", es => -_ValueOf(es.First()));
+                type("num", es => double.Parse(es.First().Value));
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # Nccc
 .Net Compiler Compiler Combinator
+
+# Example
+
+```
+:: add
+
+@set-delims '(' ')' '[' ']' '{' '}' '+' '-' '*' '/' '^'
+
+add = (@or add:(mul '+' add)
+           sub:(mul'-' add)
+           mul)
+
+mul = (@or mul:(pow '*' mul)
+           div:(pow '/' mul)
+           pow)
+
+pow = (@or pow:(primary '^' pow)
+           primary)
+
+primary = (@or par:('(' add ')')
+               par:('[' add ']')
+               par:('{' add '}')
+               neg:('-' pow)
+               num:<number>)
+```
+
+# Result Type
+
+# Grammer Overview
+
+# Scanner
+
+@set-delims '(' ')' '[' ']' '{' '}' '<' '>' '`' '::' '=' ':' '~' '??'
+@set-line-comment ';'
+@set-comment-start '#|'
+@set-comment-end '|#'
+@set-operators
+@set-quotation-marks '\''
+@set-regex-marks '/'
+@set-lisp-char
+@set-significant-whitespaces
+
+@case-sensitive on
+@split-word on
+
+# Parser
+
+### Options
+
+@left-recur-detection on|off
+左递归检测，会稍微影响性能，但是能排除
+
+@use-memorized-parser on|off
+中间结果缓存，能稍微改善Or操作的性能
+
+### Basic Parser
+
+'string'
+<type>
+<*>
+
+### Variable
+variable = (@err'invalid var' /[a-zA-Z_\-][a-zA-Z0-9_\-]*/)
+
+### Combinator
+
+seq-cmb:'@..'  ()
+
+plus-cmb:'@+'
+star-cmb:'@*'
+join-cmb:'@,*'
+join-plus-cmb:'@,+'
+or-cmb:'@or'
+not-cmb:'@!'
+maybe-cmb:'@?'
+glob-exp '~'
+
+### Named
+
+named-exp = variable ':' exp
+
+### Error
+
+err-cmb:'@err
+
+### Debug
+
+dbg-1exp = '??' exp
+dbg-exp = '[' (@+ exp) ']'
+
+# Travel AST
